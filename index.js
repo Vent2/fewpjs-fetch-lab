18afc642cc31eda6ccb130e979cc6ab84dd44e7a
Gerald Ventura <58758273+Vent2@users.noreply.github.com>
2021-08-06 20:28:16 -0400

    Done

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    // To pass the tests, don't forget to return your fetch!
+    return fetch('https://anapioficeandfire.com/api/books')
+        .then(resp => resp.json())
+        .then(data => renderBooks(data));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const main = document.querySelector('main');
+    books.forEach(book => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = book.name;
+        main.appendChild(h2);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+    fetchBooks();
 });
+
+//  The 5th book in the series 
+//  fetch('https://anapioficeandfire.com/api/books')
+// .then(resp => resp.json()) 
+// .then(json => console.log(json[4]))
